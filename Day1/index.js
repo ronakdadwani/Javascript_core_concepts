7f6c2247d0ad9183af709c3ae25cd82fb6097753
ronakdadwani <dadwanironak456@gmail.com>
2024-11-21 11:46:01 +0530

    Loops and Conditional

---
+++ Day1/index.js
@@ -0,0 +1,115 @@
+// // conditionals and loops
+
+// let userToken = undefined; // if values is not given it will run empty no executionn
+// let googelAuthToken = ""
+
+// if (userToken || googelAuthToken) {
+//     console.log("User allowed to login");
+// } else {
+//     // console.log("Please login to get your token");
+    
+// }
+// console.log("outside the scope!!~");
+
+
+
+
+
+// let isCollegeStudent = false;
+// let isCollegeEmployee = false;
+
+// if (isCollegeStudent) {
+//     console.log("Verified by College");
+    
+// } else if (isCollegeEmployee){
+//     console.log("Congrats you are college employee");
+    
+// } else {
+//     console.log("Sorry , Detail not found !");
+    
+// }
+
+// let isLOggedIn = true;
+
+// isLOggedIn ? console.log("Logout") : console.log(" Login");
+
+
+// switch and case
+
+// switch (expression) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let day = 40;
+
+// switch (day) {
+//     case 1:
+//         console.log("Mondat");
+        
+//         break;
+//     case 2:
+//             console.log("tuesday");
+            
+//             break;
+//     case 3:
+//         console.log("wednesday");
+        
+//         break;
+//     case 4:
+//         console.log("thursday");
+        
+//         break;
+//     case 5:
+//         console.log("friday");
+        
+//         break;
+
+//     default:
+//         console.log("day not found");
+        
+//         break;
+// }
+
+
+// loops 
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+let i = 1;
+
+// for( i ;  i <= 5 ; i++){
+//     console.log(`Value of i is : ${i}`);
+// }
+
+let arr = ["Superma" , "BAtman" , "internship", "labs", "webdec"]
+ for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    // console.log(arr[index]);
+    
+    
+ }
+
+ let obj = {
+    name : "ronak",
+    course : "fsjs",
+    duration : 100
+ }
+
+ // for in loop
+//  for (const i in obj) {
+//    console.log(`${i}- ${obj[i]}`);
+   
+//  }
+
+// for (const ele of arr) {
+//  console.log(ele);
+    
+// }
+

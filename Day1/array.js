cacfbdcba6f322cdb25464d8d7532b499787a1e7
ronakdadwani <dadwanironak456@gmail.com>
2024-11-21 12:47:13 +0530

    Array and its function

---
+++ Day1/array.js
@@ -0,0 +1,33 @@
+// let arr = [1,2,3,4,5,6,7,8,9];
+// console.log(arr.pop());
+
+// console.log(arr);
+
+
+// console.log(arr.indexOf(8));
+// console.log(arr.at(5));
+
+
+//shift and unshift
+
+let arr = [1,2,3,4,5,6, "g",7,"a" , 8,9];
+
+// console.log(arr.shift());
+// console.log(arr);
+
+// console.log(arr.unshift("ronk"));
+// console.log(arr);
+
+// console.log(arr.slice(5 , 8));
+// console.log(arr.splice(3 , 3));
+
+
+//reverse ad sort
+
+// console.log(arr.reverse(9));
+// console.log(arr.sort());
+
+
+
+
+
